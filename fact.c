cc144a1fed9c1b81e0cba4c46b8d718a4a3a1e00
subin <bagsubin565@gmail.com>
2025-09-22 12:23:01 +0000

    first commit

---
+++ fact.c
@@ -0,0 +1,19 @@
+#include  <stdio.h>
+int fact(int n);
+
+int global=10;
+
+int main(){
+	int a=4, b;
+	b=fact(a);
+	printf("b=%d", b);
+	global=global+b;
+}
+
+int fact(int n){
+	int result=1;
+	for (int i=1; i<=n; i++)
+		result=result*i;
+	return result;
+}
+
