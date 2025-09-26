02808c50dd97e26d56b453f0f6772594c6fab561
subin <bagsubin565@gmail.com>
2025-09-26 11:53:41 +0000

    test 2

---
+++ 2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+	int arr[10];
+	int i, sum=0;
+
+	printf("Enter 10 integers:\n");
+	for(i=0; i<10; i++){
+		scanf("%d", &arr[i]);
+	}
+
+	printf("Even numbers: ");
+	for(i=0; i<10; i++){
+		if(arr[i]%2==0){
+			printf(" %d", arr[i]);
+			sum += arr[i];
+		}
+	}
+	printf("\n");
+
+	printf("sum of even numbers = %d",sum);
+
+	return 0;
+}
+
+
