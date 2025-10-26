cb45e8f0c80a4cef87466d9ac17e6ced950e875c
subin <bagsubin565@gmail.com>
2025-10-26 14:31:03 +0000

    practice

---
+++ midterm/ch5/fsize.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#define BUFSIZE 1024
+
+int main(int argc, char *argv[])
+{
+	char buf[BUFSIZE];
+	int fd;
+	ssize_t nread;
+	long total=0;
+	
+	if((fd=open(argv[1], O_RDONLY))==-1)
+		perror(argv[1]);
+	while( (nread=read(fd, buf, BUFSIZE)) > 0)
+		total+=nread;
+	
+	close(fd);
+	printf("%s file size: %ld byte\n", argv[1], total);
+	exit(0);
+}			
---
+++ midterm/ch5/student.h
@@ -0,0 +1,7 @@
+#define MAX 24
+#define START_ID 1001001
+struct student{
+	int id;
+	char name[MAX];
+	int score;
+};	
