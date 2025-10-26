9976541571a6667c6fcfa092a988c1463a6861ac
subin <bagsubin565@gmail.com>
2025-10-26 08:55:21 +0000

    practice

---
+++ midterm/ch3/array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define N 5
+
+int main()
+{
+	int sum=0;
+	int a[N];
+	
+	for(int i=0; i<N; i++)
+		a[i]=i*2;
+	for(int i=0; i<N; i++)
+		sum+=a[i];
+		
+	printf("sum=%d\n",sum);
+}			
